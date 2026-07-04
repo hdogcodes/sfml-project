f6e8c7eb2c2e9681d8ec864c5711a162f0bde0bc
Hugo Shelton <hugoshelton@Hugos-MacBook-Air-7.local>
2026-07-04 21:18:11 +1000

    Add Player class with movement and collision

--- main.cpp
+++ main.cpp
@@ -9,21 +9,67 @@ double vecs(const double& v1, const double& v2) {
     double dif = (v1 - v2)/144;
     return dif;
 }
-//random points
-std::set<std::pair<int, int>> random(int n) {
-    std::set<std::pair<int, int>> pos;
-    for (int i = 0 ;i < n; i++) {
-        pos.insert({rand() % 900, rand() % 600});
+
+
+class Obstacle {
+public:
+    double x;
+    double y;
+    sf::Color color;
+    sf::CircleShape shape;
+
+    Obstacle(double x_, double y_, sf::Color color_, float radius = 20.f)
+        : x(x_), y(y_), color(color_)
+    {
+        shape.setRadius(radius);
+        shape.setOrigin({radius, radius}); // center origin
+        shape.setFillColor(color);
     }
-    return pos;
-}
 
-class obsticals {
-    public:
-        double x;
-        double y;
-    void draw() {
+    void draw(sf::RenderWindow& window) {
+        shape.setPosition({static_cast<float>(x), static_cast<float>(y)});
+        window.draw(shape);
+    }
 
+};
+class Player {
+public:
+    double x;
+    double y;
+    sf::Color color;
+    sf::CircleShape shape;
+
+    Player(double x_, double y_, sf::Color color_, float radius = 20.f)
+        : x(x_), y(y_), color(color_)
+    {
+        shape.setRadius(radius);
+        shape.setOrigin({radius, radius}); // center origin
+        shape.setFillColor(color);
+        shape.setPosition({static_cast<float>(x), static_cast<float>(y)});
+    }
+
+    void draw_player(sf::RenderWindow& window) {
+
+        window.draw(shape);
+    }
+    sf::Vector2f move_ment() {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+            shape.move({0.2f, 0.f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+            shape.move({-0.2f, 0.f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+            shape.move({0.f, -0.2f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+            shape.move({0.f, 0.2f});
+        }
+        sf::Vector2f pos = shape.getPosition();
+        x = pos.x;
+        y = pos.y;
+
+        return pos;
 
     }
 
@@ -37,11 +83,12 @@ int main() {
     sf::RenderWindow window(sf::VideoMode({900u, 600u}), "SFML Test");
     unsigned int width = 600;
     unsigned int height = 360;
-    const sf::Font font("/System/Library/Fonts/Supplemental/Arial.ttf");
-    sf::Text text(font, "Hello SFML", 50);
 
-    // random circle generator
-    std::set<std::pair<int, int>> randomPoints = random(50);
+    std::vector<Obstacle> stuff;
+    for (int i = 0 ; i < 50 ; i++) {
+        Obstacle thing(rand() %900, rand() %600, sf::Color(0,0,255));
+        stuff.push_back(thing);
+    }
 
 
     sf::CircleShape shape(50.f);
@@ -54,6 +101,8 @@ int main() {
     circle.setPosition({width / 2.0f, height / 2.0f});
     circle.setFillColor(sf::Color::Red );
     circle.setPointCount(35);
+    double prev_x;
+    double prev_y;
 
     while (window.isOpen()) {
         while (const std::optional event = window.pollEvent()) {
@@ -61,16 +110,17 @@ int main() {
                 window.close();
         }
         sf::Vector2f pos = shape.getPosition();
-        std::cout << pos.x << ", " << pos.y << "\n";
         sf::Vector2f pos_circle = circle.getPosition();
 
 
-        double rad_dif = std::sqrt( std::pow((pos.x- pos_circle.x), 2) + std::pow((pos.y- pos_circle.y), 2));
-        std::cout << rad_dif << "\n";
-        if (rad_dif <= 114 ) {
-            pos.x += 114 * vecs(pos.x, pos_circle.x);
-            pos.y += 114 * vecs(pos.y ,pos_circle.y);
+        if (std::pow((pos.x-300),2) + std::pow((pos.y-180),2) <= std::pow((115),2)){
+            pos.x = prev_x;
+            pos.y = prev_y;
+            std::cout << "in" <<"\n";
         }
+
+
+
 
         // bounds
         if (pos.x < 0) {
@@ -102,17 +152,15 @@ int main() {
         }
 
         window.clear();
-        for (const auto& item : randomPoints) {
-            sf::CircleShape bruh(20.f);
-            bruh.setOrigin({20.f, 20.f});
-            bruh.setPosition({static_cast<float>(item.first), static_cast<float>(item.second)});
-            bruh.setFillColor(sf::Color(100, 250, 50));
-            window.draw(bruh);
+
+        for ( auto& dude: stuff) {
+            dude.draw(window);
         }
-        window.draw(text);
         window.draw(shape);
         window.draw(circle);
         window.display();
+        prev_x = pos.x;
+        prev_y = pos.y;
     }
     return 0;
 }
